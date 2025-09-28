1bdd5b805230cbe7aa7102a00c41d8e7f5b3ab99
Abin56 <abinjohn8089@gmail.com>
2025-09-28 22:41:13 +0530

    new user bug fixed -- @edwin 28 sept

--- lib/features/auth/providers/auth_providers.dart
+++ lib/features/auth/providers/auth_providers.dart
@@ -4,7 +4,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../../core/providers/token_provider.dart';
 import '../../../core/services/firebase_auth_service.dart';
 import '../../../core/services/signin_api_service.dart';
 import '../../../core/services/token_storage_service.dart';
