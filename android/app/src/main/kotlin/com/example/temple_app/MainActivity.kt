1a9a802993bf436c5cf01d12b35fdc8612fc45bb
Abin56 <abinjohn8089@gmail.com>
2025-09-28 22:59:38 +0530

    new user bug fixed -- @edwin 28 sept

--- android/app/src/main/kotlin/com/example/temple_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/temple_app/MainActivity.kt
@@ -1,22 +1,5 @@
-package com.example.temple_app // use your actual package
+package com.example.temple_app 
 
 import io.flutter.embedding.android.FlutterActivity
-import android.os.Bundle
-import com.google.firebase.FirebaseApp
-import com.google.firebase.appcheck.FirebaseAppCheck
-import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
 
-class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // Initialize Firebase
-        FirebaseApp.initializeApp(this)
-
-        // Install Debug App Check provider
-        val firebaseAppCheck = FirebaseAppCheck.getInstance()
-        firebaseAppCheck.installAppCheckProviderFactory(
-            DebugAppCheckProviderFactory.getInstance()
-        )
-    }
-}
+class MainActivity : FlutterActivity()
