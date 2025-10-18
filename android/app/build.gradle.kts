691a343096acc82dc265401a005808c3d77573b7
Abin56 <abinjohn8089@gmail.com>
2025-10-18 21:52:42 +0530

    first commit  madhu code trying to merge  18 oct --@edwin

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
 
     defaultConfig {
         applicationId = "com.nexotech.templeapp.in"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
